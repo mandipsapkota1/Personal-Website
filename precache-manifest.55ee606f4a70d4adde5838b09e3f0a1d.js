47df074e83d4f164addc481c00b07575361a1b77
Sachin Kharel <sachink1630@gmail.com>
2022-12-03 14:06:08 +0545

    Updates

---
+++ precache-manifest.55ee606f4a70d4adde5838b09e3f0a1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "99741ffaa47df4625f4a",
+    "url": "/Personal-Website/static/css/main.ba87346b.chunk.css"
+  },
+  {
+    "revision": "99741ffaa47df4625f4a",
+    "url": "/Personal-Website/static/js/main.99741ffa.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/Personal-Website/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "fe9e49dcc9c458a203f8",
+    "url": "/Personal-Website/static/js/2.fe9e49dc.chunk.js"
+  },
+  {
+    "revision": "03282012cbbc9cbe6c80",
+    "url": "/Personal-Website/static/js/runtime~main.03282012.js"
+  },
+  {
+    "revision": "54a4a3cae19f26b84171792404ec6d28",
+    "url": "/Personal-Website/static/media/myphoto.54a4a3ca.jpg"
+  },
+  {
+    "revision": "dbf8a272257be1d3faa7c96334e4616a",
+    "url": "/Personal-Website/index.html"
+  }
+];
