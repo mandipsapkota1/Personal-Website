c843df0708c2527cb8c27de008e4f0ba2d2a1362
Sachin Kharel <sachink1630@gmail.com>
2022-12-03 13:15:22 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./myphoto.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>This is my personal website.</p>
+        <a
+          className="App-link"
+          href="https://www.instagram.com/ma_nd_ip/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Enter
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
